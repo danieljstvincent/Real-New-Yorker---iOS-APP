1d0502049eb15eb40c3360b4f15c06bb88d3cb85
Daniel <dannystvincent@gmail.com>
2021-09-09 14:01:14 -0400

    Currently, I am about to get the label to display that tells the user the question for the Quiz App. Am working towards getting the Multiple  choices Answers buttons to display.
    Also, I was able to add to temporary background until am able to finish to final version of the UI in Adobe XD

--- Real New Yorker - iOS Quiz App/Real New Yorker - iOS Quiz App/ViewController.swift
+++ Real New Yorker - iOS Quiz App/Real New Yorker - iOS Quiz App/ViewController.swift
@@ -6,16 +6,27 @@
 //
 
 import UIKit
-#import <QuartzCore/QuartzCore.h>
 
-class ViewController: UIViewController, QuizProtocol {
+
+class ViewController: UIViewController, QuizProtocol, UITableViewDelegate, UITableViewDataSource {
+
+
+    
+    @IBOutlet weak var questionLabel: UILabel!
+ 
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var model = QuizModel()
-    var questions = Question()
-    var currentQuestionIndx = 0
+    var questions = [Question]()
+    var currentQuestionIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Set Self as the delegate
+        tableView.delegate = self
+        tableView.dataSource = self
         
         model.delegate = self
         model.getQuestions()
@@ -23,8 +34,41 @@ class ViewController: UIViewController, QuizProtocol {
 // MARK: - QuizProtocol Methods
     
     func questionRetrieved(_ questions: [Question]) {
-        print("questions retrieved form model!")
+      
+        // Get a reference to the questions
+        self.questions = questions
+        
+        // Reload the tableview
+        tableView.reloadData()
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //Make sure that the questions array actually contains at question
+        
+        guard  questions.count > 0 else {return 0}
+        // Return the number of Questoin for that QUestion
+        let currentQuestion = questions[currentQuestionIndex]
+        
+        if currentQuestion.answers != nil {
+            return currentQuestion.answers!.count
+        }
+        else {
+            return 0
+        }
+    }
+    
+    // MARK: - UITableView Delegate Methods
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Get a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnswerCell", for: indexPath)
+        //Customize it
+        let label = cell.viewWithTag(1) as? UILabel
+        
+        if label != nil {
+            
+        }
+        
+ return cell
+    }
 }
 
